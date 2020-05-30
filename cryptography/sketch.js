92b7a58d310069903378cb8b22ccbec9a870cda6
vqyj <66005097+vqyj@users.noreply.github.com>
2020-05-30 16:49:13 +0800

    let input be textarea
    as per #5, but it is noot full functioning now, because it will return a space if there is ↵, which is not a good thing

--- cryptography/sketch.js
+++ cryptography/sketch.js
@@ -1,30 +1,24 @@
-let textBox;
+let vig;
 
 function setup(){
   noCanvas()
+
+
   let para = createP()
   let dec = createP()
-  textBox = createInput()
+
+
+  let textBox = createElement('textarea',"")
   textBox.input(go)
 
-  let pw = createInput()
+  let pw = createElement('textarea',"")
   pw.input(go)
 
 
 
-
-  function change(){
-    if(status == 0){
-      status = 1
-    }else{
-      status = 0
-    }
-  }
-
-
   function go(){
-    para.html(vigenere(textBox.value(), pw.value(), 0))
-    console.log(vigenere(textBox.value(), pw.value(), 1))
+    vig = vigenere(textBox.value(), pw.value(), 0)
+    para.html(vig)
     dec.html(vigenere(textBox.value(), pw.value(), 1))
   }
 
