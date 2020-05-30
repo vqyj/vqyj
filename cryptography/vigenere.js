420cafc33d925e393759de0a5ffa5c05828eb34f
vqyj <66005097+vqyj@users.noreply.github.com>
2020-05-30 10:16:24 +0800

    doc'tion, "_" in'rchang'lity, use nonlowercase
    fixed the issue of documentation or organization of the comments
    
    let underscore and space interchangable when writing in the input but will always return an underscore when you are encrypting it but will return a space when decryting it, it
    
    you can also now include non-lowercase characters, but will not perform operations with it,it is being preserved using an else function in step 4
    
    dash is now _ since underscore is not frequently use than dash
    
    fixed #3  and fixed #4

--- cryptography/vigenere.js
+++ cryptography/vigenere.js
@@ -1,66 +1,96 @@
 //VIGENERE CRYPTOGRAPHY TAKES A PLANE TEXT AND A KEY(PASSWORD),
 
-//suppose a = 0, b = 1, c = 2, d = 3 ...
-//assigning this to the plaintext, and the key.
+//suppose space = 0, a = 1, b = 2, c = 3, d = 4 ... z = 26
+
+//to encrypt it (eOrD = 0):
+//assigning this to the plaintext or input, and the key.
 // Add the number representation of plain text to the number representation of the key.
 //if the length of the key is shorter than the plaintext, use again the first number representation of the key, and so on
-//exclude the spaces, trrat them as long with no space text
-//the output which is the sum is a representation of the encrypted text, tapos na
-
-//to decrypt, minus it.
+//the output which is the sum is the num representation of the encrypted text,
 
 
-//issue the space cant be use because when there are 2 and above consecutive spaces in the return, it will be only 1
+//to decrypt (eOrD = 0):
+//do the same thing as what is above but minus the number representation insted of adding.
 
-//ISSUES, THIS FUNCTION IS NOT WORKING WHEN THERE ARE SPACES IN BOTH INPUT AND PW
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//ALGORITHM AND NAVIGATION++++++++++++++++++++++++++++++++++++++++++++
+//STEP 1, SEPARATION OF THE TEXT STRING'S CHARACTERS
+//STEP 2, ASSIGNMENT OF STRINGS TO NUMERALS
+//   will return empty when there is an error
+//   this is also the step where space will be reassign as 0, or the same as the underscore
+//STEP 3, PERFORM OPERATION OF ADDING PASSWORD TO TEXT
+//    this excludes certain characters such as non lowercase letters, but include space/dash with representation 0
+//    will return NaN if step 2 is empty
+//STEP 4, REASSIGNING NUMERALS TO ITS NEW RESPECTIVE CHARACTERS
+//    will return the original char (from step 1) if step 3 is NaN,this is executed by the else of the for loop in this step
+//    in this way, all char that are not included in the letter array will be as it is in the output without performing any operation
+//  STEP 4.1, WHEN THE CODE IS DECRYPTING, THERE IS NO WAY THAT YOU WILL NEED A DOUBLE SPACING, THUS, DASH IS NOT USEFUL HERE
+//     this for loop will make it space again all dashes since it is decrypting now
+//STEP 5, CONCATENATING ALL CHARACTERS
+
+//NOTES
+//: All the chars that is being NaN missed the operation in step3, thus all letters that must be in that spot to be added is also missed
+//  eg: input: ab+c, pw: dcba > 1,2,NaN,3 & 4,3,2,1 > perform operation: 5,6,NaN,4 >>>> b(2) is not used
+//----------------------------------------------------------------------------------------------------
+
+
 function vigenere(input,key,eOrD){
-  //0 if to encrypt
-  //1 if to decrypt
+  //0 if to encrypt;
+  //1 if to decrypt;
 
-  let text = input
-  let password = key
+  let text = input;
+  let password = key;
 
-  let sepInput = []
-  let sepPassword =[]
+//STEP 1, SEPARATION OF THE TEXT STRING'S CHARACTERS++++++++++++++++++++++++++++++++
 
-//step1, separation of stuff
+  let sepInput = [];
+  let sepPassword =[];
+
+
   for(let i = 0; i < text.length; i++){
-    sepInput[i] = text[i]
-  }
+    sepInput[i] = text[i];
+  };
 
   for(let i = 0; i < password.length; i++){
-    sepPassword[i] = password[i]
-  }
+    sepPassword[i] = password[i];
+  };
 
-//gagawin nang numbers
-  let letters = ["-", "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+//STEP 2, ASSIGNMENT OF STRINGS TO NUMERALS+++++++++++++++++++++++++++++++++++++++++++
+//    will return empty when there is an error
+//    this is also the step where space will be reassign as 0, or the same as the underscore
+  let letters = ["_", "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-  let inputNum = []
-  let passwordNum = []
+  let inputNum = [];
+  let passwordNum = [];
 
   for(let i = 0; i < text.length; i++){
     for(let j = 0; j < 27; j++){
       if(sepInput[i] == letters[j]){
-        inputNum[i] = j
+        inputNum[i] = j;
+      }else if(sepInput[i] == " "){
+        inputNum[i] = 0; //this else will let the space be just dash.
       }
     }
   }
-    // console.log(inputNum)
+
 
   for(let i = 0; i < password.length; i++){
     for(let j = 0; j < 27; j++){
       if(sepPassword[i] == letters[j]){
         passwordNum[i] = j
+      }else if(sepPassword[i] == " "){
+        passwordNum[i] = 0
       }
     }
   }
 
-  // console.log(passwordNum)
 
 
+//STEP 3, PERFORM OPERATION OF ADDING PASSWORD TO TEXT++++++++++++++++++++++++++++
+//    this excludes certain characters such as non lowercase letters, but include space/dash with representation 0
+//    will return NaN if step 2 is empty
 
-
-//adjust the inputNum by a matter of passwordNum
   let adjInputNum = []
 
 
@@ -79,23 +109,36 @@ function vigenere(input,key,eOrD){
 
 
 
+//STEP 4, REASSIGNING NUMERALS TO ITS NEW RESPECTIVE CHARACTERS+++++++++++++++++
+//    will return the original char (from step 1) if step 3 is NaN,this is executed by the else of the for loop in this step
+//    in this way, all char that are not included in the letter array will be as it is in the output without performing any operation
 
   let charsInputAgain = []
 
 
-//gagawin ng letter ung ano, ung input num
-
   for(let i = 0; i<text.length ; i++){
     if(adjInputNum[i] < 27){
       charsInputAgain[i] = letters[adjInputNum[i]]
+    }else {
+      charsInputAgain[i] = sepInput[i]
     }
   }
 
 
 
-  // console.log(charsInputAgain)
+  //STEP 4.1, WHEN THE CODE IS DECRYPTING, THERE IS NO WAY THAT YOU WILL NEED A DOUBLE SPACING, THUS, DASH IS NOT USEFUL HERE
+  //    this for loop will make it space again all dashes since it is decrypting now
+    if(eOrD == 1){
+      for(let i = 0; i<text.length ; i++){
+        if(charsInputAgain[i] == letters[0]){
+        charsInputAgain[i] = " "
+        }
+      }
+    }
 
-//pagsasamasamahin na
+
+
+//STEP 5, CONCATENATING ALL CHARACTERS+++++++++++++++++++++++++++++++++++++++
   let output;
     if(eOrD == 1){
       output  = "d ="
@@ -109,7 +152,5 @@ function vigenere(input,key,eOrD){
 
 
 
-  // console.log(output)
   return output
-  console.log(output)
 }
