8bee217cdddfa988c92e96d2c09a973a2529837a
vqyj <66005097+vqyj@users.noreply.github.com>
2020-05-30 17:34:39 +0800

    the enter or r\n is now <br> in text only
    textarea is nowfunctioning with spaces, thanks to the function from - >>https://www.codegrepper.com/code-examples/javascript/javascript+replace+line+breaks+with+br

--- cryptography/sketch.js
+++ cryptography/sketch.js
@@ -18,6 +18,15 @@ function setup(){
 
   function go(){
     vig = vigenere(textBox.value(), pw.value(), 0)
+
+    //REPLACE ENTER(\r\n|\r|\n) TO <br>
+    function nl2br(str){
+     return str.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    }
+    vig = nl2br(vig)
+
+
+
     para.html(vig)
     dec.html(vigenere(textBox.value(), pw.value(), 1))
   }
